714f8e48e0f1d30371ddfcc5a278f3647965d1cc
Diego Torres <diegotc86@gmail.com>
2019-03-07 11:46:48 -0500

    Estructura inicial

---
+++ to_do_list.rb
@@ -0,0 +1,25 @@
+class ToDo
+  def initialize
+    @task_list = []
+  end
+
+  def list
+    #PAULO
+    #Lista elementos de @task_list
+  end
+
+  def add(task)
+    #DIEGO
+    #Agrega elementos a @task_list
+  end
+
+  def delete(pos)
+    #CESAR
+    #Reemplaza un elemento de @task_list con ""
+  end
+end
+
+def input(user_input)
+  #LIAN
+  #Retorna un arreglo ["acccion", "argumento"]
+end
